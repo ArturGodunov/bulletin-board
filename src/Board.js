119fce59517d8aec16cd56296d0336eb6808ddb3
Artur Hadunou <artur.hadunou@intellectsoft.net>
2018-08-31 17:46:37 +0300

    add fetch data

--- src/Board.js
+++ src/Board.js
@@ -17,6 +17,18 @@ class Board extends Component {
         this.nextId = this.nextId.bind(this);
     }
 
+    componentWillMount() {
+        const self = this;
+
+        if (this.props.count) {
+            fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
+                .then(response => response.json())
+                .then(json => json[0]
+                    .split('. ')
+                    .forEach(sentence => self.add(sentence.substring(0, 25))));
+        }
+    }
+
     update(newText, i) {
         this.setState(prevState => ({
             notes: prevState.notes.map(
